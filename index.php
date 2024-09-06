c2ccea230af73276541675bafc09e60b2208e9c7
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-09-06 10:27:11 +0200

    [UPD] navigation button

--- index.php
+++ index.php
@@ -49,11 +49,12 @@ $hotels = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>PHP Hotels</title>
  </head>
  <body>  
      <div class="container">
-        <a href="./bonus/index.php"><button class="btn btn-primary"><i class="fas fa-folder"></i></button></a>
+        <button class="btn mt-3"><a href="./bonus/index.php"><i class="fas fa-box p-2"></i><p>BONUS</p></a></button>
         <h2 class="my-4 text-center">Lista degli Hotel</h2>
         <table class="table table-striped table-dark">
             <thead>
